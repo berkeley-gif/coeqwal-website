eb02801885830cb9721c18650fb94fa8b72a7bc5
jfantauzza <jfantauzza@berkeley.edu>
2025-03-03 05:43:33 -0800

    feature/drawer component

---
+++ apps/main/app/components/layout/Drawer.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { SwipeableDrawer, Box, List, Divider, ListItem, ListItemButton, ListItemText } from '@mui/material';
+import { useMainAppTranslation } from '../../../i18n/useMainAppTranslation';
+
+interface DrawerProps {
+  open: boolean;
+  onOpen: (event: React.KeyboardEvent | React.MouseEvent) => void;
+  onClose: (event: React.KeyboardEvent | React.MouseEvent) => void;
+}
+
+const Drawer: React.FC<DrawerProps> = ({ open, onOpen, onClose }) => {
+  const { t } = useMainAppTranslation();
+
+  const drawerList = () => (
+    <Box
+      role="presentation"
+      onClick={onClose}
+      onKeyDown={onClose}
+    >
+      <List>
+        {[
+          { text: t("drawerLinks.howWaterMoves"), href: "#how-water-moves" },
+          { text: t("drawerLinks.howWaterManaged"), href: "#how-water-managed" },
+          { text: t("drawerLinks.calSim"), href: "#calsim" },
+          { text: t("drawerLinks.climateChange"), href: "#climate-change" },
+        ].map((item, index) => (
+          <React.Fragment key={`${item.text}-${index}`}>
+            <ListItem disablePadding>
+              <ListItemButton component="a" href={item.href}>
+                <ListItemText primary={item.text} />
+              </ListItemButton>
+            </ListItem>
+            {index < 3 && <Divider />}
+          </React.Fragment>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <SwipeableDrawer
+      anchor="right"
+      open={open}
+      onClose={onClose}
+      onOpen={onOpen}
+      slotProps={{
+        backdrop: {
+          invisible: true, // Disable backdrop darkening
+        },
+      }}
+    >
+      {drawerList()}
+    </SwipeableDrawer>
+  );
+};
+
+export default Drawer;
