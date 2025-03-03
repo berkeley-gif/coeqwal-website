54cae6631e0c6b8624ae0587699e56683f43c532
jfantauzza <jfantauzza@berkeley.edu>
2025-03-03 14:15:05 -0800

    devops/updated build

--- apps/main/next.config.ts
+++ apps/main/next.config.ts
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // config, if needed
+  output: 'export', // Ensures Next.js generates static files for Amplify
+  distDir: "out", // Forces Next.js to output to "out/"
+  trailingSlash: true, // Ensures routing works correctly
+  images: {
+    unoptimized: true, // Prevents Next.js Image Optimization (Amplify doesn't support it)
+  }
 }
 
 export default nextConfig
