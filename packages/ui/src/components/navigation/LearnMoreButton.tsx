ade6defe698ba6bfd261d863ae8dfc933fe564e3
jfantauzza <jfantauzza@berkeley.edu>
2025-04-16 01:10:30 -0700

    i18n/learn more translation

--- packages/ui/src/components/navigation/LearnMoreButton.tsx
+++ packages/ui/src/components/navigation/LearnMoreButton.tsx
@@ -3,6 +3,25 @@
 import React from "react"
 import { Button, ButtonProps } from "@mui/material"
 import CustomArrowForwardIcon from "./CustomArrowForwardIcon"
+import { useTranslation } from "@repo/i18n"
+
+type ButtonTranslations = {
+  text: string
+}
+
+type TranslationsMap = {
+  en: ButtonTranslations
+  es: ButtonTranslations
+}
+
+const translations: TranslationsMap = {
+  en: {
+    text: "Learn More",
+  },
+  es: {
+    text: "Aprender Más",
+  },
+}
 
 interface LearnMoreButtonProps extends ButtonProps {
   onClick: (event: React.MouseEvent<HTMLButtonElement>) => void
@@ -14,9 +33,15 @@ export function LearnMoreButton({
   children,
   ...props
 }: LearnMoreButtonProps) {
+  const { locale, isLoading } = useTranslation()
+  
+  // Use 'en' as default until client-side hydration is complete
+  const safeLocale = !locale || isLoading ? "en" : locale
+  const componentText = translations[safeLocale as keyof TranslationsMap] || translations.en
+
   return (
     <Button onClick={onClick} endIcon={<CustomArrowForwardIcon />} {...props}>
-      {children || "Learn More"}
+      {children || componentText.text}
     </Button>
   )
 }
