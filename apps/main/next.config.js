193832d93f43eebd58b79e4ae1f7a98a520dcf05
jfantauzza <jfantauzza@berkeley.edu>
2025-04-25 12:14:41 -0700

    devops/move from static to SSR

--- apps/main/next.config.js
+++ apps/main/next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  swcMinify: true,
+  transpilePackages: [
+    "@repo/map",
+    "@repo/state",
+    "@repo/viz",
+    "@repo/ui",
+    "@repo/motion",
+    "@repo/i18n",
+    "@repo/eslint-config",
+    "@repo/typescript-config"
+  ],
+  // Enable all optimizations but keep SSR approach
+  experimental: {
+    serverActions: true,
+  }
 }
 
 export default nextConfig
