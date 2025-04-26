0d45be291fa65fe9b1708d6f0e2556dfd58cdc13
jfantauzza <jfantauzza@berkeley.edu>
2025-04-26 12:35:28 -0700

    devops/next config

--- apps/storyline-flow/next.config.js
+++ apps/storyline-flow/next.config.js
@@ -1,9 +1,19 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
+  output: 'export',  // Enable static export
+  transpilePackages: [
+    "@repo/map",
+    "@repo/state",
+    "@repo/viz",
+    "@repo/ui",
+    "@repo/motion",
+    "@repo/i18n",
+    "@repo/eslint-config",
+    "@repo/typescript-config"
+  ],
   images: {
-    unoptimized: true,
-  },
+    unoptimized: true  // Required for static export
+  }
 }
 
 export default nextConfig
