fdd76ce6b6e24831ecb3003d7c428209997d866d
jfantauzza <jfantauzza@berkeley.edu>
2025-04-29 19:41:35 -0700

    state/store slice for map animations

---
+++ packages/state/src/story/store.ts
@@ -0,0 +1,26 @@
+import { create } from "zustand"
+import { immer } from "zustand/middleware/immer"
+
+export interface StoryUIState {
+  currentScene: string | null
+  isAnimating: boolean
+  overlays: Record<string, boolean>
+  // actions
+  setScene: (id: string | null) => void
+  setAnimating: (flag: boolean) => void
+  setOverlay: (key: string, visible: boolean) => void
+}
+
+export const useStoryStore = create<StoryUIState>()(
+  immer<StoryUIState>((set) => ({
+    currentScene: null,
+    isAnimating: false,
+    overlays: {},
+    setScene: (id) => set((s) => void (s.currentScene = id)),
+    setAnimating: (flag) => set((s) => void (s.isAnimating = flag)),
+    setOverlay: (key, visible) =>
+      set((s) => {
+        s.overlays[key] = visible
+      }),
+  })),
+) 
