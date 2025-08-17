afc806e6fb1ea31f9f8a2d688e2329af86f2d5f4
jfantauzza <jfantauzza@berkeley.edu>
2025-08-17 12:08:41 -0700

    animation/scroll down

--- packages/motion/src/components/index.ts
+++ packages/motion/src/components/index.ts
@@ -1,3 +1,4 @@
 export { HeaderStory } from "./HeaderStory"
 export { Sentence, StaticSentence } from "./Sentence"
+export { ScrollIndicator } from "./ScrollIndicator"
 export * from "./divComponents"
