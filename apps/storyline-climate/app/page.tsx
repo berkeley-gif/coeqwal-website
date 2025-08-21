734d0899f4371ec0d137e896fd6d6a965eb87dad
yunhsinkuo <yskuo@ucdavis.edu>
2025-08-21 13:10:16 -0700

    Configuration panel for testing

--- apps/storyline-climate/app/page.tsx
+++ apps/storyline-climate/app/page.tsx
@@ -1,8 +1,8 @@
 "use client"
 
-import { useRef } from "react"
+import { useRef, useState } from "react"
 import { HeaderStory } from "@repo/motion/components"
-import { Box } from "@repo/ui/mui"
+import { Box, FormControl, FormControlLabel, Radio, RadioGroup,  } from "@repo/ui/mui"
 import "./main.css"
 
 import Opener from "./components/01Opener"
@@ -14,12 +14,18 @@ import SectionSupply from "./components/05Groundwater"
 import SectionResolution from "./components/08Hydroclimate"
 import Conclusion from "./components/09Conclusion"
 import Conveyance from "./components/06Conveyance"
+import { motion, useScroll, useMotionValueEvent } from "@repo/motion"
 
 export default function StoryContainer() {
   const containerRef = useRef<HTMLDivElement>(null)
+  const [useNewPlot, setUseNewPlot] = useState(false)
+
   return (
     <>
       <HeaderStory />
+      <SelectionPanel updateOnPlot={(value) => {
+        setUseNewPlot(value === "new")
+      }} />
       <Box
         component="main"
         ref={containerRef}
@@ -36,16 +42,95 @@ export default function StoryContainer() {
           pointerEvents: "none",
         }}
       >
-        <Opener />
-        <Snowmelt />
-        <Delta />
-        <Whiplash />
-        <SectionSupply />
-        <Conveyance />
-        <SectionTransition />
-        <SectionResolution />
-        <Conclusion />
+        {useNewPlot ? (
+          <>
+            <Opener />
+            <Snowmelt />
+            <SectionSupply />
+            <Delta />
+            <Conveyance />
+            <Whiplash />
+            <SectionTransition />
+            <SectionResolution />
+            <Conclusion />
+          </>
+        ) : (
+          <>
+            <Opener />
+            <Snowmelt />
+            <Delta />
+            <Whiplash />
+            <SectionSupply />
+            <Conveyance />
+            <SectionTransition />
+            <SectionResolution />
+            <Conclusion />
+          </>
+        )}
+
       </Box>
     </>
+  )
+}
+
+function SelectionPanel({
+  updateOnPlot
+}: {
+  updateOnPlot: (value: string) => void
+}) {
+  const { scrollY } = useScroll()
+  const lastYRef = useRef(0)
+  const [isHidden, setIsHidden] = useState(false)
+
+  useMotionValueEvent(scrollY, "change", (latest) => {
+    const difference = latest - lastYRef.current
+    if (Math.abs(difference) > 10) {
+      setIsHidden(difference > 0)
+    }
+    lastYRef.current = latest
+  })
+
+  return (
+    <motion.div
+      animate={isHidden ? "hidden" : "visible"}
+      variants={{
+        hidden: {
+          top: "10px",
+        },
+        visible: {
+          top: "74.5px",
+        },
+      }}
+      transition={{ duration: 0.3 }}
+      className='selection-panel'
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: 1
+      }}
+    >
+      <Box>
+        <FormControl>
+          Opener (not implemented yet)
+          <RadioGroup row name="row-radio-buttons-group" defaultValue={"oroville"}>
+            <FormControlLabel value="oroville" control={<Radio />} label="Oroville" />
+            <FormControlLabel value="temperature" control={<Radio />} label="Temperature" />
+            <FormControlLabel value="sealevel" control={<Radio />} label="Sea Level" />
+          </RadioGroup>
+        </FormControl>
+      </Box>
+      <Box>
+        <FormControl>
+          Plot
+          <RadioGroup row name="row-radio-buttons-group" defaultValue={"original"}
+            onChange={(event) => {
+              updateOnPlot(event.target.value)
+            }}>
+            <FormControlLabel value="original" control={<Radio />} label="Original" />
+            <FormControlLabel value="new" control={<Radio />} label="New" />
+          </RadioGroup>
+        </FormControl>
+      </Box>
+    </motion.div>
   )
 }
