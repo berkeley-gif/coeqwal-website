2c6fb1b451fa3595820f9229d2a353ab05079958
jfantauzza <jfantauzza@berkeley.edu>
2025-08-26 08:28:46 -0700

    styles/removed unused and useless icon

--- packages/ui/src/components/navigation/CustomArrowForwardIcon.tsx
+++
@@ -1,8 +0,0 @@
-import ArrowForwardIcon from "@mui/icons-material/ArrowForward"
-import { SvgIconProps } from "@mui/material"
-
-const ArrowForwardIconWithMargin = (props: SvgIconProps) => {
-  return <ArrowForwardIcon {...props} sx={{ ml: "1rem", ...props.sx }} />
-}
-
-export default ArrowForwardIconWithMargin
