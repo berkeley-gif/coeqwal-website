fe7d31f7abbaa58e55a8c851dba114821d55d535
jfantauzza <jfantauzza@berkeley.edu>
2025-08-30 15:46:32 -0700

    map

--- apps/main/app/page.tsx
+++ apps/main/app/page.tsx
@@ -5,6 +5,7 @@ import { Header } from "./components/Header"
 import { ConnectedMultiDrawer } from "./components/ConnectedMultiDrawer"
 import IntroSection from "./sections/IntroSection"
 import ContentPanels from "./sections/ContentPanels"
+import MapPanel from "./sections/MapPanel"
 import ScenarioExplorer from "./features/scenarioExplorer/ScenarioExplorer"
 
 export default function Home() {
@@ -35,6 +36,13 @@ export default function Home() {
       >
         {/* Panel sections */}
         <IntroSection />
+        <MapPanel 
+          mapboxToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN || "pk.eyJ1IjoiZGlnaWppbGwiLCJhIjoiY2tydmI3dXQyMDR4ajJ2cWUyd3htc3h5MCJ9.8dWK7Z9U3DfG7Ec_DP4Lww"}
+          initialLongitude={-121.4944} // California Central Valley
+          initialLatitude={37.0902}
+          initialZoom={6}
+          mapStyle="mapbox://styles/digijill/cmeum8zy2001b01s65c4d1l30"
+        />
         <ContentPanels />
         <ScenarioExplorer />
       </Box>
--- packages/map/client.ts
+++ packages/map/client.ts
@@ -2,3 +2,4 @@
 
 export { MapProviderClientWrapper as MapProvider } from "./src/context/MapProviderClientWrapper"
 export { useMap } from "./src/context/MapContext"
+export { default as Map } from "./src/Map"
