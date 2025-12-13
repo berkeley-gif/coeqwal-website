00f4bc7cc0d0088b05e432fbc1a4dbeaf40b7e1c
jfantauzza <jfantauzza@berkeley.edu>
2025-12-13 17:13:19 +0000

    devops/removed unused files

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -17,13 +17,11 @@ export type { AppMultiDrawerProps } from "./navigation/AppMultiDrawer"
 export type { GlossaryTerm, TierInfo } from "../lib/glossary"
 
 // Common components
-export { Card, ScenarioCard, ScenarioCardList } from "./common/Card"
+export { Card, ScenarioCard } from "./common/Card"
 export type { CardProps, ScenarioCardProps } from "./common/Card"
 export { Logo } from "./common/Logo"
 export { GlossaryLinkedText } from "./common/GlossaryLinkedText"
 export { LeadingMarkerText } from "./common/LeadingMarkerText"
-export { MoreButton } from "./common/MoreButton"
-export type { MoreButtonProps } from "./common/MoreButton"
 export { ArrowHead } from "./icons/ArrowHead"
 export { RoundedDownArrow } from "./icons/RoundedDownArrow"
 export type { RoundedDownArrowProps } from "./icons/RoundedDownArrow"
@@ -36,8 +34,6 @@ export { MapMarkerTooltip } from "./common/MapMarkerTooltip"
 export type { MapMarkerTooltipProps } from "./common/MapMarkerTooltip"
 export { InfoTooltip } from "./common/InfoTooltip"
 export type { InfoTooltipProps } from "./common/InfoTooltip"
-export { HelpTooltip } from "./common/HelpTooltip"
-export type { HelpTooltipProps } from "./common/HelpTooltip"
 
 // New standardized tooltip system (in tooltips/)
 export { ClickTooltip } from "./common/tooltips/ClickTooltip"
@@ -46,30 +42,17 @@ export { HybridTooltip } from "./common/tooltips/HybridTooltip"
 export type { HybridTooltipProps } from "./common/tooltips/HybridTooltip"
 export { Dropdown } from "./common/Dropdown"
 export type { DropdownProps, DropdownOption } from "./common/Dropdown"
-export { SimpleSelect } from "./common/SimpleSelect"
-export type {
-  SimpleSelectProps,
-  SimpleSelectOption,
-} from "./common/SimpleSelect"
 export { DiscreteSlider } from "./common/DiscreteSlider"
 export type { DiscreteSliderProps } from "./common/DiscreteSlider"
 export { InfoIconButton } from "./common/InfoIconButton"
 export type { InfoIconButtonProps } from "./common/InfoIconButton"
 export { SortButton } from "./common/SortButton"
 export type { SortButtonProps } from "./common/SortButton"
-export { CustomDropdown } from "./common/CustomDropdown"
-export type { CustomDropdownProps } from "./common/CustomDropdown"
-export { SectionHeader } from "./common/SectionHeader"
-export type { SectionHeaderProps } from "./common/SectionHeader"
 export { StyledTextInput } from "./common/StyledTextInput"
 export type { StyledTextInputProps } from "./common/StyledTextInput"
 
 // Panel components
-export { BasePanel } from "./panels/BasePanel"
 export { OneColumnPanel } from "./panels/OneColumnPanel"
-export { TwoColumnPanel } from "./panels/TwoColumnPanel"
-export { VideoPanel } from "./panels/VideoPanel"
-export type { BasePanelProps } from "./panels/BasePanel"
 export { Spacer } from "./panels/Spacer"
 export {
   DashboardPanel,
@@ -88,7 +71,6 @@ export {
   DocumentCheckedIcon,
   DocumentExpandedIcon,
   DocumentCollapsedIcon,
-  MapIcon,
 } from "./icons/DocumentIcons"
 
 // Call-response UI components
