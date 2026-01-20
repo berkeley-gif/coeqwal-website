36874573737a2d11fc216da7bee7e3107cbcefab
jfantauzza <jfantauzza@berkeley.edu>
2026-01-20 00:28:59 +0000

    task/add error boundaries via @repo/utils package

--- apps/main/app/components/tabPanels/Explore.tsx
+++ apps/main/app/components/tabPanels/Explore.tsx
@@ -4,8 +4,21 @@
  * Renders the ScenarioExplorer component for the Explore tab.
  */
 
+import { ErrorBoundary } from "@repo/utils"
+import { ErrorFallback } from "@repo/ui"
 import ScenarioExplorer from "../../features/scenarioExplorer/ScenarioExplorer"
 
 export default function ExplorePanel() {
-  return <ScenarioExplorer />
+  return (
+    <ErrorBoundary
+      fallback={
+        <ErrorFallback
+          title="Explorer couldn't load"
+          message="This might be a temporary issue. Try refreshing the page."
+        />
+      }
+    >
+      <ScenarioExplorer />
+    </ErrorBoundary>
+  )
 }
