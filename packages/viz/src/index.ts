493368070f6c9c9b29d15a81c8f07d6e1e036a8a
jfantauzza <jfantauzza@berkeley.edu>
2026-03-24 20:38:25 +0000

    viz/added reminder about d3 export

--- packages/viz/src/index.ts
+++ packages/viz/src/index.ts
@@ -9,7 +9,10 @@ export type {
   ScenarioComparisonChartProps,
 } from "./types"
 
-// Export d3
+// TODO: Remove this re-export once apps/storyline-flow imports d3 directly
+// (e.g. `import { select } from "d3"` instead of `import { d3 } from "@repo/viz"`).
+// Re-exporting the full D3 namespace through the barrel forces Turbopack to
+// parse every D3 submodule on first compile, adding ~10-15s to dev startup.
 export * as d3 from "d3"
 
 // Export components
