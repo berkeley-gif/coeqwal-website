af81934611f5fbaf3676fe031323ecc4a9fb56ed
yuya737 <ykawakami@ucdavis.edu>
2026-07-17 11:00:07 -0700

    fix: remove duplicate CATEGORY_COLORS def

--- packages/viz/src/components/TierGrid.tsx
+++ packages/viz/src/components/TierGrid.tsx
@@ -93,18 +93,6 @@ const DEFAULT_TIER_COLORS = {
   "Tier 3": "#f2944f",
   "Tier 4": "#ee5d32",
 }
-const CATEGORY_COLORS = [
-  "#4e79a7", // Blue
-  "#f28e2b", // Orange
-  "#e15759", // Red
-  "#76b7b2", // Cyan
-  "#59a14f", // Green
-  "#edc948", // Yellow
-  "#b07aa1", // Purple
-  "#ff9da7", // Pink
-  "#9c755f", // Brown
-  "#bab0ac", // Gray
-]
 
 const CATEGORY_COLORS = [
   "#4e79a7", // Blue
