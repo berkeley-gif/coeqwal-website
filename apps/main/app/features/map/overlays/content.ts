623320cd4a293cc22c89d89381d3e696016fb186
melijimenez11 <melijimenez@berkeley.edu>
2026-08-05 22:49:31 -0500

    updated the key outcomes panel content - not including panel's clickable content

--- apps/main/app/features/map/overlays/content.ts
+++ apps/main/app/features/map/overlays/content.ts
@@ -83,60 +83,50 @@ export const HYDROCLIMATES: ReadonlyArray<HydroclimateFuture> = [
 
 export interface KeyOutcome {
   outcomeCode: OutcomeCode
-  title: string
   description: string
 }
 
 export const KEY_OUTCOMES: ReadonlyArray<KeyOutcome> = [
   {
     outcomeCode: "CWS_DEL",
-    title: "Community water deliveries",
     description:
-      "Reliability of water supplies to communities to satisfy essential drinking water needs",
+      "Reliability of surface water deliveries to communities to satisfy existing potable demands",
   },
   {
     outcomeCode: "AG_REV",
-    title: "Agricultural revenue",
     description:
       "Economic productivity of agricultural crops based on water availability",
   },
   {
     outcomeCode: "ENV_FLOWS",
-    title: "Environmental flows",
     description:
       "Seasonal patterns of river flows needed to support healthy ecosystems",
   },
   {
     outcomeCode: "DELTA_ECO",
-    title: "Delta estuary ecology",
     description:
       "Seasonal patterns of flows needed to support the health of the Bay Delta estuary",
   },
   {
     outcomeCode: "WRC_SALMON_AB",
-    title: "Winter-run salmon",
     description:
       "Population status of Sacramento River winter-run Chinook salmon",
   },
   {
     outcomeCode: "FW_DELTA_USES",
-    title: "Freshwater for in-Delta uses",
     description:
       "Availability of freshwater in the Delta to support local communities and farms",
   },
   {
     outcomeCode: "FW_EXP",
-    title: "Freshwater for Delta exports",
-    description: "Availability of freshwater for export to other regions",
+    description: "Volume of availability of freshwater for export to other regions",
   },
   {
     outcomeCode: "RES_STOR",
-    title: "Reservoir storage",
     description: "Levels of water stored in major reservoirs",
   },
   {
     outcomeCode: "GW_STOR",
-    title: "Groundwater storage",
     description: "Amount and trends of water stored in groundwater basins",
   },
 ] as const
