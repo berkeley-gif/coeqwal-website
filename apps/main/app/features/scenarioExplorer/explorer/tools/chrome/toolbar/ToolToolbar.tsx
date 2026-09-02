da8c0848307471829553274fbaf76f051a107ebc
Eric Lehmer <elehmer@berkeley.edu>
2026-09-02 08:30:41 -0700

    tweak columnGap of View by HC info icon

--- apps/main/app/features/scenarioExplorer/explorer/tools/chrome/toolbar/ToolToolbar.tsx
+++ apps/main/app/features/scenarioExplorer/explorer/tools/chrome/toolbar/ToolToolbar.tsx
@@ -134,7 +134,7 @@ export default function ToolToolbar({
                 flexWrap: "wrap",
               }}
             >
-              <Box sx={{ display: "flex", alignItems: "center" }}>
+              <Box sx={{ display: "flex", alignItems: "center", columnGap: 0.5 }}>
                 <InfoIconButton
                   tooltipContent={
                     <>
